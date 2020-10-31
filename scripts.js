92cdd37b922d0038b240df48a69f74579e419d14
Patrece <patrececampbell5@gmail.com>
2020-10-31 18:56:15 -0500

    Added code to the scripts.js file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+    var response = document.getElementById("email");
+    var submit = document.querySelector(".newsletter .btn");
+    displaymessage = document.getElementsByClassName("message")[0];
+    submit.addEventListener('click', handlesubmission);
+
+    function handlesubmission(clickEvent){
+        if (response.value == ""){
+            displaymessage.innerHTML = "Please enter a valid email address.";
+            console.log("Please enter a valid email address.");
+        }
+        else{
+            displaymessage.innerHTML = "Thank you! Your email address " + response.value + " has been added to our mailing list!";
+            console.log("Thank you! Your email address " + response.value + " has been added to our mailing list!");
+        }
+        clickEvent.preventDefault();
+    }
+}
